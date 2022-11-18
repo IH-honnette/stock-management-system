fca52f351c96bd21e3fbc68a3276dd2ca472bc15
IH-honnette <ihozohonnette12@gmail.com>
2022-11-18 08:31:51 +0200

    Initial commit

---
+++ Server/stock-mgt-sytem/src/main/java/com/estock/stockmgtsytem/StockMgtSytemApplication.java
@@ -0,0 +1,13 @@
+package com.estock.stockmgtsytem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockMgtSytemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockMgtSytemApplication.class, args);
+	}
+
+}
